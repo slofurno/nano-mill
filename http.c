da67cdd7787ac454d936984f6a28c06d32ee2153
slofurno <slofurno@gmail.com>
2015-11-20 15:36:39 -0500

    forgive me...

--- http.c
+++ http.c
@@ -28,11 +28,10 @@ void free_gif_request(gif_request *s)
     free_slice(s->data);
 }
 
-int min(int n, int m){
+int minimum(int n, int m){
     if (n >= m){
         return m;
     } 
-    
     return n;  
 }
 
@@ -49,7 +48,6 @@ void dump_bytes(char *bytes, int len){
 coroutine void subscribe(gif_request *request)
 {
     int sub = nn_socket(AF_SP,NN_SUB);
-    printf("sub on fd: %d\n",sub);
     assert(sub>=0);
 
     char *buf= NULL;
@@ -63,7 +61,6 @@ coroutine void subscribe(gif_request *request)
 
     assert(nn_getsockopt(sub,NN_SOL_SOCKET,NN_RCVFD,&fd,&fd_sz)>=0);
 
-    printf("waiting for job to finish\n");
     int events;
     int nbytes=-1;
     
@@ -82,7 +79,7 @@ coroutine void subscribe(gif_request *request)
 
     if (nbytes>0){
         printf("rec %d bytes\n",nbytes);
-        printf("job done: %s\n",buf);
+       // printf("job done: %s\n",buf);
     }else if(nbytes==0){
         printf("how do we rec 0 bytes??\n");
     }else{
@@ -127,22 +124,18 @@ coroutine void start_collector(chan workers, const int worker_router){
 
         events = fdwait(fd,FDW_IN,-1);
         if (events & FDW_IN){
-            printf("fd d for sub fd d signaled..\n");
         }else{
             printf("fd error??\n");
         }
         
-        printf("worker coming in\n");
         int rc = nn_recvmsg(worker_router, &hdr, NN_DONTWAIT);
         assert(rc>=0);
-        printf("fuck yuou\n");
 
-        printf("msg is: %.*s\n", rc, (char*)hdr.msg_iov->iov_base);
+//        printf("msg is: %.*s\n", rc, (char*)hdr.msg_iov->iov_base);
         
         if (rc >= 100){
             memcpy(uuid, body, sizeof(char)*36);
             sprintf(directory, "tmp/REAL-%s.gif", uuid);
-            printf("lets announce job is done: %s\n", uuid);
 
             FILE *f = fopen(directory, "w");
             fwrite(body+36, sizeof(char), rc-36, f);
@@ -160,14 +153,11 @@ coroutine void start_router(chan workers, int worker_router, chan jobs)
     void *buf = NULL;
 
     while(1){
-        printf("waiting for next request\n");
         gif_request *job = chr(jobs, gif_request*);
-        printf("literally, what?\n");
-        printf("producing request id: %s\n", job->uuid->bytes);    
+        //printf("producing request id: %s\n", job->uuid->bytes);    
 
         size_t sum = job->uuid->len-1 + job->data->len;
         
-        printf("allocating %d\n",sum);
         buf = nn_allocmsg (sum, 0);
        // buf = malloc(sizeof(char)*sum);
         assert(buf != NULL);
@@ -192,9 +182,7 @@ coroutine void start_router(chan workers, int worker_router, chan jobs)
         hdr.msg_iov = &iovec;
         hdr.msg_iovlen = 1;
 
-        printf("about to send work\n");
         nn_sendmsg(worker_router, &hdr, NN_DONTWAIT);
-        printf("work sent\n");
     }
 }
 
@@ -228,58 +216,85 @@ coroutine void handle_conn(tcpsock conn, chan queue){
             method = malloc(first+1);
             url = malloc(second-first+1);  
             sprintf(method, "%.*s", first, buf);
-            sprintf(url, "%.*s", second-first, buf+first+1);
-            //printf("is this your method?, %s, %s\n", method, url); 
+            sprintf(url, "%.*s", second-first-2, buf+first+2);
         }
         else if (nbytes>= header_len && strncmp(buf, header, header_len)==0){
             char *rawlen = buf+header_len;
             content_length = atoi(rawlen);
-            //printf("body size: %d\n",content_length);
         }else if(nbytes==2){
             break;
         }
         n++;
     }
 
-
     uuid_t uuid;
     char *u = malloc(37);
     uuid_generate(uuid);
     uuid_unparse(uuid, u);
 
-    char *okres ="HTTP/1.1 200 OK\r\nContent-Length: 36\r\nContent-Type: text/plain; charset=utf-8\r\n\r\n";
+    char *okres ="HTTP/1.1 200 OK\r\nContent-Length: %d\r\nContent-Type: text/html; charset=utf-8\r\nAccess-Control-Allow-Origin: *\r\nAccess-Control-Allow-Headers: Origin, X-Requested-With, Content-Type, Accept, Key, Cache-Control\r\n\r\n";
+
+    if (strcmp(url, "api/listen") != 0){
+        printf("%s\n", url);
+        slice *result = make_slice();
+        
+        FILE *index = fopen(url,"r");
+        if (index == NULL){
+            goto done;
+        }
+
+        while((nbytes = fread(buf, sizeof(char), 4096, index)) > 0){
+           printf("appending\n");
+           append(result, buf); 
+        }
+
+        int hdrlen = sprintf(buf, okres, result->len);
+        tcpsend(conn, buf, hdrlen, -1);
+        tcpsend(conn, result->bytes, result->len, -1);
+        tcpflush(conn, -1);
+        goto done;
+    }
+
+
     nbytes = tcpsend(conn, okres, strlen(okres)+1, -1);
     tcpsend(conn, u, strlen(u), -1);
     tcpflush(conn, -1);
     //printf("wrote response\n"); 
     //printf("looking for content length: %d\n",content_length);
+   
+    printf("%s\n",url); 
+    printf("%s\n",buf);
 
     char *file = malloc(content_length);
     char *filep = file;
+    printf("is the file len: %d\n",content_length);
     size_t left = content_length;
     while(left>0){
-        nbytes = tcprecv(conn, filep, left, -1);
+        int min = minimum(left, 4096);
+        nbytes = tcprecv(conn, filep, min, -1);
         filep+=nbytes; 
         left-=nbytes;
         //printf("%d bytes left\n", left);
     }
 
+    if (content_length>200){
 
-    gif_request *request = malloc(sizeof(gif_request));
-    slice *data = malloc(sizeof(slice));
-    data->bytes = file;
-    data->len =content_length; 
+        gif_request *request = malloc(sizeof(gif_request));
+        slice *data = malloc(sizeof(slice));
+        data->bytes = file;
+        data->len =content_length; 
 
-    slice *id = malloc(sizeof(slice));
-    id->bytes=u;
-    id->len=37;
-    request->data = data;
-    request->uuid = id;
+        slice *id = malloc(sizeof(slice));
+        id->bytes=u;
+        id->len=37;
+        request->data = data;
+        request->uuid = id;
 
-    chs(queue,gif_request*, request);
-    go(subscribe(request));
-    tcpclose(conn);
-    //dump_bytes(file, content_length);
+        chs(queue,gif_request*, request);
+        go(subscribe(request));
+    }
+
+done: tcpclose(conn);
 }
 
 
@@ -289,7 +304,6 @@ int main(int argc, char **argv){
         printf("%s\n",argv[1]);
     }
 
-    printf("running http listener...\n");
     ipaddr addr = iplocal(NULL, 80, 0);
     tcpsock ls = tcplisten(addr, 10);
 
@@ -304,13 +318,9 @@ int main(int argc, char **argv){
     nn_bind(worker_router, WORKERROUTER);
     assert(nn_setsockopt(worker_router, NN_SOL_SOCKET, 
                 NN_RCVMAXSIZE, &rcv_max, sizeof(rcv_max)) >= 0);
-    printf("options set...\n");
 
     go(start_router(chdup(workers), worker_router, chdup(jobs)));
-    printf("router started\n");
-
     go(start_collector((workers), worker_router));
-    printf("skipping collector?\n");
 
     while(1){
         tcpsock s = tcpaccept(ls, -1);
